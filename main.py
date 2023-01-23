36df32da3e812d006adbb8065fccb7cace27f48b
jamie <jamieallen050401@gmail.com>
2023-01-23 13:22:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+#################################################################################################
+# Program:  Python/SQLite Web Application TODO - Ammend the name where necessary ...            #
+# Filename: main.py                                                                             #
+# Author:   Jamie Allen                                                                         #
+# Course:   BSc Digital Technology and Solutions                                                #
+# Module:   Software Engineering and Agile                                                      #
+# Version:  1.0                                                                                 #
+# Date:     xx/xx/23                                                                            #
+#################################################################################################
+
+import sys as System
+# TODO - Import other default python modules here ...
+
+# TODO - Import other packages here ...
+
+if __name__ == '__main__':
+    0
+
+#################################################################################################
+# File: main.py                                                                                 #
+#                                                                                               #
+# Disclaimer: The following source code is the sole work of the author unless otherwise stated. #
+#                                                                                               #
+# Copyright (C) Jamie Allen. All Rights Reserved.                                               #
+#################################################################################################
+
+#################################################################################################
+# References:                                                                                   #
+#                                                                                               #
+# Python is used throughout this application. Other Packages are as follows:                    #
+#                                                                                               #
+# Python (xx/xx/xx) TODO - Finish Python reference here ...                                     #
+#                                                                                               #
+# TODO - Add references to other packages here ...                                              #
+#                                                                                               #
+# References used within this application are as follows:                                       #
+#                                                                                               #
+# TODO - Add other references here ...                                                          #
+#                                                                                               #
+#################################################################################################
