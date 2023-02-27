672f00c984d6533bbba2d7a2e0aa7c2666833083
jamie <jamieallen050401@gmail.com>
2023-02-27 14:48:57 +0000

    Updated deploy script

--- jenkins/deploy-application.sh
+++ jenkins/deploy-application.sh
@@ -3,4 +3,4 @@
 export DOCKER_HOST_NONPROD="tcp://10.0.0.10:2375"
 
 echo "Executing docker-compose file ..."
-docker-compose -H $DOCKER_HOST_NONPROD up -d
+docker -H $DOCKER_HOST_NONPROD compose up -d
