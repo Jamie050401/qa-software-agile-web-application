d323a06ac556b5a727fcce2c880baab0cb130337
jamie <jamieallen050401@gmail.com>
2023-04-03 11:04:16 +0100

    Implemented ticket editing functionality

--- web/views.py
+++ web/views.py
@@ -11,7 +11,6 @@
 
 from datetime import datetime
 from math import trunc
-from sqlalchemy import DateTime
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from web.authentication.auth import current_user as AuthUser
@@ -71,8 +70,54 @@ def tickets():
     database.close()
 
     if current_user.is_authenticated:
-        return render_template("tickets.html", user=current_user, create=False, tickets=tickets_in_db, get_user_profile_image=get_user_profile_image, convert_ticket_created_datetime=convert_ticket_created_datetime)
+        return render_template("tickets.html", user=current_user, create=False, edit=False, tickets=tickets_in_db, get_user_profile_image=get_user_profile_image, convert_ticket_created_datetime=convert_ticket_created_datetime, edit_ticket=edit_ticket)
     return redirects(display_login=True)
+
+def get_users():
+    database = session_local()
+    users = database.query(User).all()
+    database.close()
+    return users
+
+@views.route('/tickets/edit', methods=["GET", "POST"])
+def edit_ticket():
+    # Getting the ticket ID as an argument from the URL
+    ticket_id = request.args['ticket_id']
+    # Fetching the ticket from the database
+    database = session_local()
+    ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
+    database.close()
+
+    if ticket is None:
+        flash("Ticket not found!", category="failure")
+        return redirect(url_for("views.tickets"))
+
+    # Displaying the edit screen to the user
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            return render_template("tickets.html", user=current_user, create=False, edit=True, ticket=ticket, get_users=get_users)
+        return redirects(display_login=True)
+    # Updating the ticket in the database
+    else:
+        database = session_local()
+
+        assignee = request.form.get("assignee")
+        team = request.form.get("team")
+        issue_type = request.form.get("type_of_issue")
+        issue_desc = request.form.get("issue_desc")
+        database.delete(ticket)
+
+        ticket.assignee_id = assignee
+        ticket.team = team
+        ticket.issue_type = issue_type
+        ticket.issue_desc = issue_desc
+        database.add(ticket)
+
+        database.commit()
+        flash("Ticket updated!", category="success")
+        database.close()
+
+        return redirect(url_for("views.tickets"))
 
 @views.route('/tickets/create', methods=["GET", "POST"])
 def create_ticket():
@@ -95,33 +140,33 @@ def create_ticket():
                 database.commit()
                 flash("Ticket created!", category="success")
                 database.close()
-                return render_template("tickets.html", user=current_user)
+                return redirect(url_for("views.tickets"))
         else:
             flash("Failed to create ticket! Specified user does not exist.", category="failure")
         database.close()
 
     if current_user.is_authenticated:
-        return render_template("tickets.html", user=current_user, create=True)
+        return render_template("tickets.html", user=current_user, create=True, edit=False)
     return redirects(display_login=True)
 
-@views.route('/users')
-def users():
-    return display("users.html", display_login=True)
+#@views.route('/users')
+#def users():
+#    return display("users.html", display_login=True)
 
-@views.route('/users/create')
-def create_user():
-    if current_user.is_authenticated:
-        return render_template("users.html", user=current_user, create=True)
+#@views.route('/users/create')
+#def create_user():
+#    if current_user.is_authenticated:
+#        return render_template("users.html", user=current_user, create=True)
+#
+#    return redirects(display_login=True)
 
-    return redirects(display_login=True)
+#@views.route('/profile')
+#def profile():
+#    return display("profile.html", display_login=True)
 
-@views.route('/profile')
-def profile():
-    return display("profile.html", display_login=True)
-
-@views.route('/settings')
-def settings():
-    return display("settings.html", display_login=True)
+#@views.route('/settings')
+#def settings():
+#    return display("settings.html", display_login=True)
 
 #################################################################################################
 # File: views.py                                                                                #
