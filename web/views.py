6d1d57afd84eb3ed1245c2d89cbe5dd8a39d9750
jamie <jamieallen050401@gmail.com>
2023-04-10 18:32:52 +0100

    Various changes
    1. Finalised ticket deletion behaviour (with confirmation prompt)
    2. Tightened up backend logic to ensure only authenticated users can carry out actions
    3. Improved backend logic behind ticket deletion to ensure only 'Admin' users can delete tickets

--- web/views.py
+++ web/views.py
@@ -62,11 +62,10 @@ def convert_ticket_created_datetime(creation_date):
 
 @views.route('/tickets')
 def tickets():
-    database = session_local()
-    tickets_in_db = database.query(Ticket).all()
-    database.close()
-
     if current_user.is_authenticated:
+        database = session_local()
+        tickets_in_db = database.query(Ticket).all()
+        database.close()
         return render_template("tickets.html", user=current_user, create=False, view=False, edit=False, tickets=tickets_in_db, get_user_profile_image=get_user_profile_image, convert_ticket_created_datetime=convert_ticket_created_datetime, edit_ticket=edit_ticket)
     return redirects(display_login=True)
 
@@ -76,20 +75,53 @@ def get_user(user_id):
     database.close()
     return user
 
+@views.route('/tickets/create', methods=["GET", "POST"])
+def create_ticket():
+    if current_user.is_authenticated:
+        if request.method == "POST":
+            database = session_local()
+
+            email = request.form.get("email")
+            if email == "":
+                email = current_user.email
+            team = request.form.get("team")
+            issue_type = request.form.get("typeOfIssue")
+            issue_desc = request.form.get("issueDescription")
+
+            ticket_user = database.query(User).filter(User.email == email).first()
+            if ticket_user:
+                user_id = ticket_user.id
+                new_ticket = Ticket(user_id, team, issue_type, issue_desc)
+                if new_ticket.is_valid:
+                    database.add(new_ticket)
+                    database.commit()
+                    flash("Ticket created!", category="success")
+                    database.close()
+                    return redirect(url_for("views.tickets"))
+            else:
+                flash("Failed to create ticket! Specified user does not exist.", category="failure")
+            database.close()
+        return render_template("tickets.html", user=current_user, create=True, view=False, edit=False)
+    return redirects(display_login=True)
+
 @views.route('/tickets/view')
 def view_ticket():
-    # Getting the ticket ID as an argument from the URL
-    ticket_id = request.args['ticket_id']
-    # Fetching the ticket from the database
-    database = session_local()
-    ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
-    database.close()
-
-    if ticket is None:
-        flash("Ticket not found!", category="failure")
-        return redirect(url_for("views.tickets"))
-
     if current_user.is_authenticated:
+        # Getting the ticket ID as an argument from the URL
+        ticket_id = request.args.get('ticket_id', None)
+        if ticket_id is None:
+            flash("Ticket not found!", category="failure")
+            return redirect(url_for("views.tickets"))
+
+        # Fetching the ticket from the database
+        database = session_local()
+        ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
+        database.close()
+
+        if ticket is None:
+            flash("Ticket not found!", category="failure")
+            return redirect(url_for("views.tickets"))
+
         return render_template("tickets.html", user=current_user, create=False, view=True, edit=False, ticket=ticket, get_user=get_user)
     return redirects(display_login=True)
 
@@ -101,72 +133,75 @@ def get_users():
 
 @views.route('/tickets/edit', methods=["GET", "POST"])
 def edit_ticket():
-    # Getting the ticket ID as an argument from the URL
-    ticket_id = request.args['ticket_id']
-    # Fetching the ticket from the database
-    database = session_local()
-    ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
-    database.close()
-
-    if ticket is None:
-        flash("Ticket not found!", category="failure")
-        return redirect(url_for("views.tickets"))
-
-    # Displaying the edit screen to the user
-    if request.method == "GET":
-        if current_user.is_authenticated:
-            return render_template("tickets.html", user=current_user, create=False, view=False, edit=True, ticket=ticket, get_users=get_users)
-        return redirects(display_login=True)
-    # Updating the ticket in the database
-    else:
-        database = session_local()
-
-        assignee = request.form.get("assignee")
-        team = request.form.get("team")
-        issue_type = request.form.get("type_of_issue")
-        issue_desc = request.form.get("issue_desc")
-        database.delete(ticket)
-
-        ticket.assignee_id = assignee
-        ticket.team = team
-        ticket.issue_type = issue_type
-        ticket.issue_desc = issue_desc
-        database.add(ticket)
-
-        database.commit()
-        flash("Ticket updated!", category="success")
-        database.close()
-
-        return redirect(url_for("views.tickets"))
-
-@views.route('/tickets/create', methods=["GET", "POST"])
-def create_ticket():
-    if request.method == "POST":
-        database = session_local()
-
-        email = request.form.get("email")
-        if email == "":
-            email = current_user.email
-        team = request.form.get("team")
-        issue_type = request.form.get("typeOfIssue")
-        issue_desc = request.form.get("issueDescription")
-
-        ticket_user = database.query(User).filter(User.email == email).first()
-        if ticket_user:
-            user_id = ticket_user.id
-            new_ticket = Ticket(user_id, team, issue_type, issue_desc)
-            if new_ticket.is_valid:
-                database.add(new_ticket)
-                database.commit()
-                flash("Ticket created!", category="success")
-                database.close()
-                return redirect(url_for("views.tickets"))
-        else:
-            flash("Failed to create ticket! Specified user does not exist.", category="failure")
-        database.close()
-
     if current_user.is_authenticated:
-        return render_template("tickets.html", user=current_user, create=True, view=False, edit=False)
+        # Getting the ticket ID as an argument from the URL
+        ticket_id = request.args.get('ticket_id', None)
+        if ticket_id is None:
+            flash("Ticket not found!", category="failure")
+            return redirect(url_for("views.tickets"))
+
+        # Fetching the ticket from the database
+        database = session_local()
+        ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
+        database.close()
+
+        if ticket is None:
+            flash("Ticket not found!", category="failure")
+            return redirect(url_for("views.tickets"))
+
+        # Displaying the edit screen to the user
+        if request.method == "GET":
+            return render_template("tickets.html", user=current_user, create=False, view=False, edit=True, ticket=ticket, get_users=get_users)
+        # Updating the ticket in the database
+        else:
+            database = session_local()
+
+            assignee = request.form.get("assignee")
+            team = request.form.get("team")
+            issue_type = request.form.get("type_of_issue")
+            issue_desc = request.form.get("issue_desc")
+            database.delete(ticket)
+
+            ticket.assignee_id = assignee
+            ticket.team = team
+            ticket.issue_type = issue_type
+            ticket.issue_desc = issue_desc
+            database.add(ticket)
+
+            database.commit()
+            flash("Ticket updated!", category="success")
+            database.close()
+
+            return redirect(url_for("views.tickets"))
+    return redirects(display_login=True)
+
+@views.route('/tickets/delete', methods=["GET"])
+def delete_ticket():
+    if current_user.is_authenticated:
+        if current_user.role_name == "Admin":
+            # Getting the ticket ID as an argument from the URL
+            ticket_id = request.args.get('ticket_id', None)
+            if ticket_id is None:
+                flash("Ticket not found!", category="failure")
+                return redirect(url_for("views.tickets"))
+
+            # Fetching the ticket from the database
+            database = session_local()
+            ticket = database.query(Ticket).filter(Ticket.id == ticket_id).first()
+            database.close()
+
+            if ticket is None:
+                flash("Ticket not found!", category="failure")
+                return redirect(url_for("views.tickets"))
+
+            database = session_local()
+            database.delete(ticket)
+            database.commit()
+            flash("Ticket deleted!", category="success")
+            database.close()
+            return redirect(url_for("views.tickets"))
+        flash("Only 'Admin' users can delete tickets!", category="failure")
+        return redirect(url_for("views.tickets"))
     return redirects(display_login=True)
 
 #@views.route('/users')
